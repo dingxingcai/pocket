dfdaf7f06d7a9c93392b18487e862176dc969bee
dingxc <775862230@qq.com>
2018-04-21 10:43:45 +0800

    新增总计消费会员数量

--- laravel/app/GraphQL/Query/VipQuery.php
+++ laravel/app/GraphQL/Query/VipQuery.php
@@ -47,6 +47,16 @@ class VipQuery extends Query
         //七天新增的总会员数量
         $vips = DB::connection('sqlsrv')->select("select top 7 CONVERT(varchar(10), CreateDate, 23) as 'date', count(*) as 'vipNums' from nVipCardSign where createDate < CONVERT(varchar(30),getdate(),23)  GROUP BY CreateDate order by CreateDate desc;");
 
+        //统计有消费行为的会员数量
+        $buyVips = DB::connection('sqlsrv')->select("select count(t.total) as buyNums from (select n.VipCardID ,count(n.VipCardID) as total from nVipCardSign n left join BillIndex b on 
+n.VipCardID = b.VipCardID where b.VipCardID != -1  group by n.VipCardID) t;");
+        $buys = [
+            'date' => '有消费行为会员数',
+            'vipNums' => $buyVips[0]->buyNums
+        ];
+        $vips[] = $buys;
+
+        //统计总的会员数量
         $total = NVipCardSign::select('VipCardID')->count();
         $info = [
             'date' => '总计会员数',
